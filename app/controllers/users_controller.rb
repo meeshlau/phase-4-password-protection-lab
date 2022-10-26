877a6315880d349c35a2dc61d41ff6a8e9e47d33
Michelle Lau <meeshklau@gmail.com>
2022-10-25 14:39:18 -1000

    commit tree 7b187a06c864bb188ec14effd26c358cec1b9768

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :user_data_invalid
+  rescue_from ActiveRecord::RecordNotFound, with: :user_data_not_found
+
+  def show
+    current_user = User.find(session[:user_id])
+    render json: current_user
+
+  end
+
+  def create
+    user = User.create!(user_params)
+    session[:user_id] ||= user.id
+    render json: user, status: :created
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :password_confirmation)
+  end
+
+  def user_data_invalid(error_hash)
+    render json: { errors: error_hash.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def user_data_not_found
+    render json: { error: "Please login to view your account page." }, status: :unauthorized
+  end
+end
